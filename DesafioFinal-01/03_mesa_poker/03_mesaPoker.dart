9fb06fbe998ce50427f69605d3579c02373ac33f
Marcela Guedes <marcellauguedes@gmail.com>
2022-01-14 00:07:03 -0300

    Terminei  questão Poker

--- DesafioFinal-01/03_mesa_poker/03_mesaPoker.dart
+++ DesafioFinal-01/03_mesa_poker/03_mesaPoker.dart
@@ -1,23 +1,38 @@
+class jogadores {
+  final String nome;
+  final int valor;
 
+  jogadores(this.nome, this.valor);
+}
 
 void main() {
-  final valorMinimo = 200;
-  final valorMaximo = 1000;
-  List jogadores = [
-    {'nome': 'João', 'valor': 300},
-    {'nome': 'Denise', 'valor': 500},
-    {'nome': 'Roberto', 'valor': 100},
-    {'nome': 'Pedro', 'valor': 800},
-    {'nome': 'Alice', 'valor': 180}
-  ];
-  var jogadoresLiberado = [];
+//Dados dos jogadores
+  final jogador1 = jogadores('Jõao', 300);
+  final jogador2 = jogadores('Denise', 500);
+  final jogador3 = jogadores('Roberto', 100);
+  final jogador4 = jogadores('Pedro', 800);
+  final jogador5 = jogadores('Alice', 180);
 
-  for (var i = 0; i < jogadores.length; i++) {
-    if (jogadores[i].valor < valorMinimo || jogadores[i].valor > valorMaximo) {
-      jogadores.remove(jogadores[i]);
-    } else {
-      jogadoresLiberado.add(jogadores[i]);
-    }
+//listagem dos jogadores
+  final listaJogadores = [jogador1, jogador2, jogador3, jogador4, jogador5];
+
+//Valor requisito
+  double valorMinimo = 200;
+  double valorMaximo = 1000;
+
+//mapeando a lista
+
+  var jogadoresLiberado = [];
+  for (var i = 0; i < listaJogadores.length; i++) {
+    if (listaJogadores[i].valor < valorMaximo &&
+        listaJogadores[i].valor > valorMinimo) {
+      jogadoresLiberado.add(listaJogadores[i].nome);
+    } ;
   }
-  print(jogadoresLiberado);
+
+  //imprimindo resultado
+  String statusDaPartida =
+      "Valor mínimo da mesa: R\$ $valorMinimo\nValor máximo da mesa: R\$ $valorMaximo\n\nJogadores da mesa:\n\n${jogadoresLiberado.join("\n")}";
+
+  print(statusDaPartida);
 }
